82b068caac4735863f510af39ab7c344ee4cf79c
osamariar@hotmail.com <osamariar@hotmail.com>
2022-12-19 13:31:27 -0800

    Pushing code upto adding roles'

---
+++ Database/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) not null,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) not null,
+    salary DECIMAL not null,
+    department_id INT not null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) not null,
+    last_name VARCHAR(30) not null,
+    role_id INT not null,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role (id)
+)
